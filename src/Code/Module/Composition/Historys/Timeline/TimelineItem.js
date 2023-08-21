30320e5b8eb29d3afe56b2176045eae345a796b8
zfzhangfei <1179241059@qq.com>
2023-08-21 18:08:34 +0800

    标签

--- src/Code/Module/Composition/Historys/Timeline/TimelineItem.js
+++ src/Code/Module/Composition/Historys/Timeline/TimelineItem.js
@@ -1,17 +1,17 @@
 import { Card } from "antd";
+import { HeartFilled } from "@ant-design/icons";
 
 const TimelineItem = ({ date, title, content }) => {
   return (
     <div className="timeline-item">
       <div className="timeline-date">{date}</div>
-      <h3 className="timeline-title">·</h3>
+      <h3 className="timeline-title" ><HeartFilled style={{fontSize:'32px'}}/></h3>
       <div className="timeline-content">
         <Card
           hoverable
-          style={{ width: 240}}
-          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          style={{ width: 300 }}
+          // cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
         >
-          {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
           {content}
         </Card>
       </div>
