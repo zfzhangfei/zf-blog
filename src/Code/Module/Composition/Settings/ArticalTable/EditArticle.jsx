beb8e60b17f1f95262f319f247ece6371d3b1343
zfzhangfei <1179241059@qq.com>
2023-08-31 14:02:14 +0800

    保存文章

--- src/Code/Module/Composition/Settings/ArticalTable/EditArticle.jsx
+++ src/Code/Module/Composition/Settings/ArticalTable/EditArticle.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import TextbusEditor from '../../../../../Plugin/Textbus/TextbusEditor'
 import { postArtical, getArtical, editArtical } from '../../../Api/Api';
-import { Button, Typography, Select } from 'antd';
+import { Button, Typography, Select ,message} from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import DemoEditor from '../../../../../Utils/MdEditor';
 import MyParagraph from '../../../../CommonComponent/MyParagraph';
 import SelectTags from '../../../../CommonComponent/SelectTags';
 import VditorEditor from '../../../../../Utils/VditorEditor';
+import dayjs from 'dayjs';
 
 export default class EditArticle extends Component {
 
@@ -15,14 +16,14 @@ export default class EditArticle extends Component {
         ParagraphValue: '123',
         artical: [],
         currentArtical: null,
-        currentSelectMark: null,
+        // currentSelectMark: null,
     }
 
     componentDidMount = async () => {
         this.setState({
             artical: await getArtical(),
             currentArtical: this.props.location.state,
-            currentSelectMark: this.props.location.state.Mark
+            // currentSelectMark: this.props.location.state.Mark
         })
 
 
@@ -62,11 +63,43 @@ export default class EditArticle extends Component {
     // }
 
     getSelectMark = (value) => {
-        console.log('====================================');
-        console.log(value);
-        console.log('====================================');
-        this.setState({
-            currentSelectMark: value.join('/')
+        // console.log('====================================');
+        // console.log(value);
+        // console.log('====================================');
+        // this.setState({
+        //     currentSelectMark: value.join('/')
+        // })
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                currentArtical: {
+                    ...prevState.currentArtical,
+                    Mark: value.join('/')
+                }
+            }
+        });
+    }
+
+    saveArticle = (articleContent) => {
+        //    console.log(articleContent,'articleContent');
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                currentArtical: {
+                    ...prevState.currentArtical,
+                    Content: articleContent
+                }
+            }
+        });
+    }
+
+    handleEditorSave() {
+        const time = dayjs().format('HH:mm:ss');
+        editArtical(this.state.currentArtical.Name, this.state.currentArtical.Mark, this.state.currentArtical.Content, this.state.currentArtical.Id).then(() => {
+            message.success('保存成功！'+time); 
+        }).catch(() => {
+            message.error('保存失败！'+time);
+
         })
     }
 
@@ -76,6 +109,7 @@ export default class EditArticle extends Component {
                 {/* <Button onClick={() => { this.handleArtical() }} block type='primary'><PlusOutlined /></Button> */}
                 <div style={{ width: '100%', height: 140 }}>
                     <SelectTags getSelectMark={this.getSelectMark} currentArtical={this.state.currentArtical}></SelectTags>
+                    <Button onClick={() => { this.handleEditorSave() }} block type='primary'>保存</Button> 
                     <div className='ArticalSummary'>
 
                     </div>
@@ -83,12 +117,12 @@ export default class EditArticle extends Component {
 
                     </div>
                 </div>
-                <div style={{ display: 'inline-block', verticalAlign: 'top', width: '100%',height:'calc(100vh - 200px)'}}>
+                <div style={{ display: 'inline-block', verticalAlign: 'top', width: '100%', height: 'calc(100vh - 200px)' }}>
                     <div className='ArticalContent'>
                         {
                             this.state.currentArtical ?
                                 // <DemoEditor currentArtical={this.state.currentArtical} currentSelectMark={this.state.currentSelectMark}></DemoEditor> : ''
-                                <VditorEditor currentArtical={this.state.currentArtical}></VditorEditor>:''
+                                <VditorEditor currentArtical={this.state.currentArtical} saveArticle={this.saveArticle}></VditorEditor> : ''
                         }
                     </div>
 
--- src/Utils/VditorEditor.js
+++ src/Utils/VditorEditor.js
@@ -2,20 +2,25 @@ import "vditor/dist/index.css";
 import React from "react";
 import Vditor from "vditor";
 
-const VditorEditor = ({currentArtical}) => {
+const VditorEditor = ({ currentArtical, saveArticle }) => {
   const [vd, setVd] = React.useState('1');
 
   React.useEffect(() => {
     const vditor = new Vditor("vditor", {
-      height:"calc(100vh - 200px)",
+      height: "calc(100vh - 200px)",
       after: () => {
         vditor.setValue(currentArtical.Content);
         setVd(vditor);
       },
+      input(value) {
+        // 输入变化时保存当前值 
+        saveArticle(value);
+      },
     });
   }, []);
 
-  return <div id="vditor" className="vditor" height="1000px"/>;
+
+  return <div id="vditor" className="vditor" height="1000px" />;
 };
 
 export default VditorEditor;
