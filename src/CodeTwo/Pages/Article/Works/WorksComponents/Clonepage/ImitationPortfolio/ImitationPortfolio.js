2d6915cfaa0391ce3c86a00715762ebac9c780fa
zfzhangfei <96964403+zfzhangfei@users.noreply.github.com>
2023-11-08 18:01:57 +0800

    services

--- src/CodeTwo/Pages/Article/Works/WorksComponents/Clonepage/ImitationPortfolio/ImitationPortfolio.js
+++ src/CodeTwo/Pages/Article/Works/WorksComponents/Clonepage/ImitationPortfolio/ImitationPortfolio.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './ImitationPortfolio.scss'
 import NavBar from './Components/navbar/NavBar'
+import Hero from './Components/hero/Hero'
+import Parallax from './Components/parallax/Parallax'
+import Services from './Components/services/Services'
 
 export default function ImitationPortfolio() {
     return (
         <div id='ImitationPortfolio'> 
-            <section><NavBar></NavBar></section>
-            <section>Hero</section>
-            <section>Hero</section>
-            <section>Hero</section>
+            <section id='Homepage'>
+                <NavBar></NavBar>
+                <Hero></Hero>
+                </section>
+            <section id='Services'><Parallax type="services"/></section>
+            <section><Services></Services></section>
+            <section id='Portfolio'><Parallax type="portfolio"/></section>
             <section>Hero</section>
             <section>Hero</section>
             <section>Hero</section>
