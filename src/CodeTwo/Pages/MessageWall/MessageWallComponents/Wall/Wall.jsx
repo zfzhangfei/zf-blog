8d298fe7e4e0a358be550c56403792ed38ae19e3
zfzhangfei <96964403+zfzhangfei@users.noreply.github.com>
2023-11-27 18:01:19 +0800

    留言墙

--- src/CodeTwo/Pages/MessageWall/MessageWallComponents/Wall/Wall.jsx
+++ src/CodeTwo/Pages/MessageWall/MessageWallComponents/Wall/Wall.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Wall.scss'
+import { Image } from 'antd'
 
 const Wall = () => {
   return (
     <div className='Wall'>
-      Wall
+      <Image src='/CodeTwo/Wall/111.Webp' style={{width:300,height:300}}></Image>
     </div>
   )
 }
